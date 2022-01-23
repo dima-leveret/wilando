7c8753293d7317b597d089825851464edd800480
Dima <dimazayac@gmail.com>
2022-01-23 16:17:46 +0100

    added some text to the main and about pages

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,16 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>Strona Główna</h1>
+        <h1>Witamy w świece unikalnych technologij dekoru!</h1>
+        <p>
+          Chcesz dodać w swoje życie barw czy inspiracji? Wyróżnić swoje
+          mieszkanie lub miejsce pracy? Zapraszamy do współpracy z naszym
+          zespołem Wilando. Prezentujemy Wam nową możliwość dekorowania ścian i
+          innych powierzchni za pomocą pionowej drukarki. Teraz logo Waszej
+          firmy, zdjęcie rodzinne, pejzaż lub znany na całym świece obraz może
+          ozdobić ściany Waszego domu, biura, restauracji czy innego
+          pomieszczenia. Jedynym ograniczeniem jest twoja własna wyobrażnia.
+        </p>
       </main>
     </div>
   );
