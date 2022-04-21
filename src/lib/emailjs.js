98568d3185594b01a9da4d5eec3e7fbc4e75ce25
dima-leveret <dimazayac@gmail.com>
2022-04-21 11:06:18 +0200

    added wilando env variables

--- src/lib/emailjs.js
+++ src/lib/emailjs.js
@@ -2,12 +2,25 @@ import emailjs from "emailjs-com";
 
 export const sendMail = (formData) => {
     emailjs
+
+      //Dima dev form data
+
+      // .sendForm(
+      //   `${process.env.NEXT_PUBLIC_DIMA_EMAILJS_SERVICE_ID}`,
+      //   `${process.env.NEXT_PUBLIC_DIMA_EMAILJS_TEMPLATE_ID}`,
+      //   formData,
+      //   `${process.env.NEXT_PUBLIC_DIMA_EMAILJS_PUBLIC_KEY}`
+      // )
+
+      //Wilando form data
+
       .sendForm(
-        `${process.env.NEXT_PUBLIC_DIMA_EMAILJS_SERVICE_ID}`,
-        `${process.env.NEXT_PUBLIC_DIMA_EMAILJS_TEMPLATE_ID}`,
+        `${process.env.NEXT_PUBLIC_WILANDO_EMAILJS_SERVICE_ID}`,
+        `${process.env.NEXT_PUBLIC_WILANDO_EMAILJS_TEMPLATE_ID}`,
         formData,
-        `${process.env.NEXT_PUBLIC_DIMA_EMAILJS_PUBLIC_KEY}`
+        `${process.env.NEXT_PUBLIC_WILANDO_EMAILJS_PUBLIC_KEY}`
       )
+
       .then(
         (result) => {
           console.log(result.text);
