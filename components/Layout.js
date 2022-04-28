e3ac363459707f927c46171734ea2191390f82cc
dima-leveret <dimazayac@gmail.com>
2022-04-28 13:23:49 +0200

    fix conflict

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,6 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import styles from "../styles/Layout.module.css"
-import styles from "../styles/Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
