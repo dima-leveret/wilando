589f7ec01dfff2f8dcdddd22e7b3131498c4b5e2
dima-leveret <dimazayac@gmail.com>
2022-07-03 20:17:57 +0100

    added icons to contact form

--- components/EmailJsForm.js
+++ components/EmailJsForm.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import styles from "../styles/Components/EmailJsForm.module.css";
 import emailjs from "emailjs-com";
+import * as FaIcons from "react-icons/fa";
 
 function EmailJsForm() {
   const form = useRef();
@@ -56,43 +57,51 @@ function EmailJsForm() {
 
       <div className={styles.formContainer}>
         <form className={styles.form} onSubmit={handleSubmit} ref={form}>
-          {/* <label htmlFor="name">Name</label> */}
-          <input
-            id="name"
-            type="text"
-            required
-            name="name"
-            placeholder="Name"
-            className={styles.input}
-          />
+          <div className={styles.inputContainer}>
+            <input
+              id="name"
+              type="text"
+              required
+              name="name"
+              placeholder="Name"
+              className={styles.input}
+            />
+            <FaIcons.FaUser />
+          </div>
 
-          {/* <label htmlFor="email">Email</label> */}
-          <input
-            id="email"
-            type="email"
-            required
-            name="email"
-            placeholder="Email"
-            className={styles.input}
-          />
+          <div className={styles.inputContainer}>
+            <input
+              id="email"
+              type="email"
+              required
+              name="email"
+              placeholder="Email"
+              className={styles.input}
+            />
+            <FaIcons.FaEnvelope />
+          </div>
 
-          {/* <label htmlFor="phone">Phone number</label> */}
-          <input
-            id="phone"
-            type="tel"
-            name="phone"
-            placeholder="Phone"
-            className={styles.input}
-          />
+          <div className={styles.inputContainer}>
+            <input
+              id="phone"
+              type="tel"
+              name="phone"
+              placeholder="Phone"
+              className={styles.input}
+            />
+            <FaIcons.FaPhoneAlt />
+          </div>
 
-          {/* <label htmlFor="message">Message</label> */}
-          <textarea
-            id="message"
-            type="text"
-            name="message"
-            placeholder="Message"
-            className={styles.textarea}
-          />
+          <div className={styles.textareaContainer}>
+            <textarea
+              id="message"
+              type="text"
+              name="message"
+              placeholder="Message"
+              className={styles.textarea}
+            />
+            <FaIcons.FaPencilAlt className={styles.pencilIcon} />
+          </div>
 
           <input type="submit" value="Send" className={styles.btn} />
         </form>
