796f641d4eb14db2d6f4e01ad1284227c8d97a47
dima-leveret <dimazayac@gmail.com>
2022-10-23 16:03:45 +0100

    add price list page with styles

--- pages/price-list.js
+++ pages/price-list.js
@@ -2,7 +2,7 @@ import HeadComponent from "../components/HeadComponent";
 import PageTitle from "../components/PageTitle";
 import styles from "../styles/pages/Price-list.module.css";
 import { IoIosArrowForward } from "react-icons/io";
-import Link from "next/link";
+import { MoreInfo } from "../components/MoreInfo";
 
 function PriceList() {
   return (
@@ -10,35 +10,17 @@ function PriceList() {
       <HeadComponent title="Wilando | Price list" />
       <PageTitle title="Cennik" />
       <div className={styles.container}>
+        <span>
+          Cena każdego projektu wyceniana jest indywidualnie w zależności od
+          wielu czynników takich jak rozmiar grafiki czy miejscowość itp.
+        </span>
+        <span>
+          Szacunkowy koszt druku do 1m<sup>2</sup> wynosi 300 zł netto + VAT (8%
+          dla osób prywatnych oraz 23% dla firm). Cena jest wyliczona dla
+          powiatu poznańskiego. Dla tego dla wyceny prosimy o podanie
+          miejscowości gdzie ma być wykonany druk.
+        </span>
         <div className={styles.pricing}>
-          <span>
-            Cena każdego projektu wyceniana jest indywidualnie w zależności od
-            wielu czynników takich jak rozmiar grafiki czy miejscowość itp.
-          </span>
-          <span>
-            Szacunkowy koszt druku do 1m2 wynosi 300 zł netto + VAT (8% dla osób
-            prywatnych oraz 23 % dla firm). Cena jest wyliczona dla powiatu
-            poznańskiego. Dla tego dla wyceny prosimy o podanie miejscowości
-            gdzie ma być wykonany druk.{" "}
-          </span>
-          <p>
-            Po więcej szczegółów zapraszamy do kontaktu przez nasz{" "}
-            <Link href="/contact">
-              <a className={styles.contactFormLink}>formularz kontaktowy</a>
-            </Link>{" "}
-            oraz przez nasze strony na{" "}
-            <a
-              className={styles.facebookLink}
-              href="https://www.facebook.com/Wilando.print"
-              target="blank"
-            >
-              Facebook
-            </a>{" "}
-            lub{" "}
-            <a className={styles.instagramLink} href="#" target="blank">
-              Instagram
-            </a>{" "}
-          </p>
           <h4 className={styles.pricingTitle}>Dla wyceny potrzebujemy:</h4>
           <ul className={styles.procingList}>
             <li>
@@ -67,6 +49,7 @@ function PriceList() {
             </li>
           </ul>
         </div>
+        <MoreInfo />
       </div>
     </>
   );
