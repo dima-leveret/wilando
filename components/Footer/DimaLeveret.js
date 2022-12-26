da29a2bef1abad793302013b863e1a9d50ee71a4
dima-leveret <dimazayac@gmail.com>
2022-12-26 12:18:35 +0100

    add my information to footer

--- components/Footer/DimaLeveret.js
+++ components/Footer/DimaLeveret.js
@@ -1,3 +1,21 @@
+import styles from "../../styles/Components/Footer.module.css";
+import { Typography } from "../Typography";
+import { FaLinkedin, FaFacebook } from "react-icons/fa";
+
 export const DimaLeveret = () => {
-  return <p>Created by Dima Zaichenko</p>;
+  return (
+    <div className={styles.dimaContainer}>
+      <Typography styleVariant="textPrimary">
+        Created by Dima Zaichenko
+      </Typography>
+      <div className={styles.dimaSocialLinks}>
+        <a href="https://www.linkedin.com/in/dima-zaichenko/" target="_blank">
+          <FaLinkedin className={styles.dimaLinkedin} />
+        </a>
+        <a href="https://www.facebook.com/dima.zaychenko.9" target="_blank">
+          <FaFacebook className={styles.dimaFacebook} />
+        </a>
+      </div>
+    </div>
+  );
 };
